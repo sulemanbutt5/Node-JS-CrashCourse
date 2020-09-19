3f59a4b2b77189ce5877b4aa0dacd8bba3ff3758
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-09-19 23:18:25 +0500

    Get, Post, Delete, Route Parameters

--- app.js
+++ app.js
@@ -1,91 +1,91 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 // express app
-const app = express();
+const app = express()
 
 // connect to mongodb & listen for requests
-const dbURI = 'mongodb+srv://forblogs:test1234@node-tuts.owfqp.mongodb.net/node-tuts?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://forblogs:test1234@node-tuts.owfqp.mongodb.net/node-tuts?retryWrites=true&w=majority'
 
-mongoose.connect(dbURI, {useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err))
 
 // register view engine
-app.set('view engine', 'ejs');
-
+app.set('view engine', 'ejs')
 
 // middleware & static files
-app.use(express.static('public'));
-app.use(morgan('dev'));
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
 app.use((req, res, next) => {
-  res.locals.path = req.path;
-  next();
-});
+  res.locals.path = req.path
+  next()
+})
 
-// mongoose & mongo tests
-app.get('/add-blog', (req, res) => {
-  const blog = new Blog({
-    title: 'Suleman\'s blog',
-    snippet: 'This is written by Suleman',
-    body: 'Dolore cupidatat elit labore laborum. Elit ex adipisicing fugiat dolor do pariatur voluptate aute dolor occaecat do tempor occaecat. Sunt eiusmod officia duis nostrud occaecat.Ullamco culpa consequat minim aliquip. Nostrud Lorem sint sint consequat nostrud eiusmod anim laboris ea nisi elit ipsum adipisicing. Fugiat eiusmod aute velit dolor occaecat cupidatat nulla eiusmod consectetur ut duis non adipisicing. Deserunt enim eu aliqua ullamco nostrud eu magna et velit laborum fugiat. Dolore Lorem reprehenderit officia aliqua magna.'
-  })
-
-  blog.save()
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.get('/all-blogs', (req, res) => {
-  Blog.find()
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.get('/single-blog', (req, res) => {
-  Blog.findById('5f66300696a7903ef43476a2')
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
+// routes
 app.get('/', (req, res) => {
-  res.redirect('/blogs');
-});
+  res.redirect('/blogs')
+})
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
-});
+  res.render('about', { title: 'About' })
+})
 
 // blog routes
 app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
+  res.render('create', { title: 'Create a new blog' })
+})
 
 app.get('/blogs', (req, res) => {
   Blog.find().sort({ createdAt: -1 })
     .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
+      res.render('index', { blogs: result, title: 'All blogs' })
     })
     .catch(err => {
-      console.log(err);
-    });
-});
+      console.log(err)
+    })
+})
+
+app.post('/blogs', (req, res) => {
+  // console.log(req.body)
+  const blog = new Blog(req.body)
+
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id
+  
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
-});
+  res.status(404).render('404', { title: '404' })
+})
