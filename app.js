2a27b9f6733c91de364e7d8ab1cf97e3edade2db
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-09-19 14:41:17 +0500

    View Engines (EJS, Passing data into views, partials)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 
 // express app
 const app = express();
@@ -6,22 +6,28 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    {title: 'Suleman speaks', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Shaban ignores', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Asad listens', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a New Blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/FZF.html', { root: __dirname })
-})
+  res.status(404).render('404', { title: '404' });
+});
